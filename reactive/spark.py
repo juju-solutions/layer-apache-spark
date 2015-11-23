e5ecdad7be8e97efd06f0aa5228c8727d374cffd
Cory Johns <johnsca@gmail.com>
2015-11-23 18:39:01 -0500

    Added comment documenting the states and whence they came

--- reactive/spark.py
+++ reactive/spark.py
@@ -3,6 +3,35 @@ from charms.reactive import set_state
 from charmhelpers.core import hookenv
 
 
+# This file contains the reactive handlers for this charm.  These handlers
+# determine when the various relations and other conditions are met so that
+# Spark can be deployed.  The states used by this charm to determine this are:
+#
+#   * bootstrapped - This is set by the bigdata base layer once all of the
+#                    library dependencies are installed.
+#                    (https://github.com/juju-solutions/layer-bigdata)
+#
+#   * spark.installed - This is set by this charm in the code below.
+#
+#   * hadoop.connected - This is set by the hadoop-plugin interface layer once
+#                        the plugin subordinate charm is connected to both this
+#                        charm and the Hadoop core cluster.  The prefix "hadoop"
+#                        in this state is determined by the name of the relation
+#                        to the plugin charm in metadata.yaml.
+#                        (https://github.com/juju-solutions/interface-hadoop-plugin)
+#
+#   * hadoop.yarn.ready - This is set by the hadoop-plugin interface layer once
+#                         Yarn has reported that it is ready to run jobs.  The
+#                         prefix "hadoop"in this state is determined by the name of
+#                         the relationto the plugin charm in metadata.yaml.
+#
+#   * hadoop.hdfs.ready - This is set by the hadoop-plugin interface layer once
+#                         HDFS has reported that it is ready to store data.  The
+#                         prefix "hadoop"in this state is determined by the name
+#                         of the relationto the plugin charm in metadata.yaml.
+#
+
+
 def dist_config():
     from jujubigdata.utils import DistConfig  # no available until after bootstrap
     if not getattr(dist_config, 'value', None):
